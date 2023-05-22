2cd30b6075721635fa5ba146cab3997df2af490e
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-22 21:40:24 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pins: ["Poppins", "sans-serif"],
+      },
+      maxWidth: {
+        size: "6.5rem",
+      },
+      colors: {
+        pink: "#FFE4E0",
+        blue: "#1000CB",
+        lightBlue: "#301FFD",
+        text: "#373737",
+      },
+    },
+  },
+  plugins: [],
+};
