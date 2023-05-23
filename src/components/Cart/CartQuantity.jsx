f2bd6d9be56b03d32f306824645f9d647032fa8c
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-23 22:03:00 +0200

    update detail page with quantity

--- src/components/Cart/CartQuantity.jsx
+++ src/components/Cart/CartQuantity.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const CartQuantity = ({ amount, setDecrease, setIncrease }) => {
+const CartQuantity = () => {
+  const [amount, setAmount] = useState(1);
+  function setDecrease() {
+    amount > 1 ? setAmount(amount - 1) : setAmount(1);
+  }
+  function setIncrease() {
+    if (amount < 1) {
+    }
+    setAmount(amount + 1);
+  }
   return (
     <>
-      <div className="flex">
-        <div>
-          <button onClick={() => setDecrease()}>
+      <div className=" container mx-auto border-b-2 py-6 mb-4 ">
+        <div className=" flex justify-around ">
+          <button onClick={setDecrease}>
             <FaMinus />
           </button>
-          <div className="font-bold text-1xl"> {amount} </div>
-          <button onClick={() => setIncrease()}>
+          <div className="font-bold text-1xl text-primary"> {amount} </div>
+          <button onClick={setIncrease}>
             <FaPlus />
           </button>
         </div>
+      </div>
+      <div className="p-6 pt-0 my-4 ">
+        <Link to="/cart">
+          <button className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-2 border-primary ">
+            Add to Cart
+          </button>
+        </Link>
       </div>
     </>
   );
--- src/components/detailCard/SingleItem.jsx
+++ src/components/detailCard/SingleItem.jsx
@@ -45,18 +45,12 @@ const SingleItem = ({ product }) => {
           <div className="flex flex-col">
             <h2 className=" font-bold "> People Review </h2>
 
-            <p className="font-pins">
+            <p className="font-pins border-b-2 py-6 mb-4">
               <Review reviews={reviews} />
             </p>
           </div>
           <div>
             <CartQuantity />
-          </div>
-
-          <div className="p-6 pt-0 my-4 ">
-            <button className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-2 border-primary ">
-              Add to Cart
-            </button>
           </div>
         </div>
       </section>
