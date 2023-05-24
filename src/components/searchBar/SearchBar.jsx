8f37b6ffd1b1e1a24339b73c1e400a3cd07f6790
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-24 19:55:56 +0200

    update

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { useContext } from "react";
 import { ProductContext } from "../../context/ProductContext";
-import ApiMap from "../../api/ApiMap";
 
-const SearchBar = ({ onSearch }) => {
+const SearchBar = ({ product }) => {
   const [text, setText] = useState("");
   console.log(text);
   const { products } = useContext(ProductContext);
@@ -11,29 +10,21 @@ const SearchBar = ({ onSearch }) => {
   const filterProducts = products.filter((items) => {
     return items.title.trim().toLowerCase().includes(text);
   });
-
-  // const handleSearch = (searchText) => {
-  //   setText(searchText);
-  // };
-
-  // <ApiMap filterProducts={filterProducts} />;
-
   console.log(filterProducts);
-  // const SearchBar = ({ onSearch }) => {
-  //   const handleInputChange = (e) => {
-  //     onSearch(e.target.value);
-  //   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor  ">
-      <form className="w-full max-w-md" onSubmit={SearchBar}>
+      <form className="w-full max-w-md" onSubmit={onSubmit}>
         <div className="flex items-center  border-b-2 border-primary py-2">
           <input
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             name="text"
             value={text}
-            onChange={(e) => setText()}
+            onChange={(e) => setText(e.target.value)}
             placeholder="Search..."
           />
         </div>
