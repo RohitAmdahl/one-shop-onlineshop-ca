f9982550d77f3166c221d403a33382767bcdf13d
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-24 19:53:31 +0200

    updating

--- src/api/ApiMap.jsx
+++ src/api/ApiMap.jsx
@@ -1,20 +1,14 @@
 import { ProductContext } from "../context/ProductContext";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Card from "../components/card/Card";
-
-const ApiMap = () => {
-  const [text, setText] = useState("");
+import SearchBar from "../components/searchBar/SearchBar";
+const ApiMap = ({ filterProducts }) => {
   const { products } = useContext(ProductContext);
-  const filterProducts = products.filter((items) => {
-    return items.title.trim().toLowerCase().includes(text);
-  });
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
 
   return (
     <>
-      <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor  ">
+      <SearchBar />
+      {/* <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor  ">
         <form className="w-full max-w-md" onSubmit={onSubmit}>
           <div className="flex items-center  border-b-2 border-primary py-2">
             <input
@@ -27,10 +21,10 @@ const ApiMap = () => {
             />
           </div>
         </form>
-      </div>
+      </div> */}
       <div className=" container max-w-sm p-10   md:max-w-xl lg:max-w-4xl  mx-auto">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {filterProducts.map((product) => {
+          {products.map((product) => {
             return <Card key={product.id} product={product} />;
           })}
         </div>
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable array-callback-return */
 import React, { useState } from "react";
 import { useContext } from "react";
 import { ProductContext } from "../../context/ProductContext";
+import ApiMap from "../../api/ApiMap";
 
-const SearchBar = ({ product }) => {
+const SearchBar = ({ onSearch }) => {
   const [text, setText] = useState("");
   console.log(text);
   const { products } = useContext(ProductContext);
@@ -11,21 +11,29 @@ const SearchBar = ({ product }) => {
   const filterProducts = products.filter((items) => {
     return items.title.trim().toLowerCase().includes(text);
   });
+
+  // const handleSearch = (searchText) => {
+  //   setText(searchText);
+  // };
+
+  // <ApiMap filterProducts={filterProducts} />;
+
   console.log(filterProducts);
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
+  // const SearchBar = ({ onSearch }) => {
+  //   const handleInputChange = (e) => {
+  //     onSearch(e.target.value);
+  //   };
 
   return (
     <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor  ">
-      <form className="w-full max-w-md" onSubmit={onSubmit}>
+      <form className="w-full max-w-md" onSubmit={SearchBar}>
         <div className="flex items-center  border-b-2 border-primary py-2">
           <input
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             name="text"
             value={text}
-            onChange={(e) => setText(e.target.value)}
+            onChange={(e) => setText()}
             placeholder="Search..."
           />
         </div>
