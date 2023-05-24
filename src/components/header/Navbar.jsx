51ec300d58a2b40566c297c72ecf8de9ddcec098
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-24 22:44:26 +0200

    nothing much change just test css clases

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -14,12 +14,12 @@ const NavBar = () => {
       <nav className="text-fontcolor font-pins ">
         <div className="text-center py-3 border-b-2 border-y-pink w-1/2 mx-auto">
           <ul className="hidden md:flex  container mx-auto lg:flex lg: justify-evenly lg:items-start max-w-xl">
-            <li className="p-2 hover:text-indigo-800 hover:underline font-medium">
+            <li className="p-2   hover:text-indigo-800 hover:underline font-medium">
               <NavLink className="" to="/">
                 Home
               </NavLink>
             </li>
-            <li className="p-2 hover:text-indigo-800 hover:underline font-medium  ">
+            <li className="p-2  hover:text-indigo-800 hover:underline font-medium  ">
               <NavLink className="" to="/product">
                 Products
               </NavLink>
