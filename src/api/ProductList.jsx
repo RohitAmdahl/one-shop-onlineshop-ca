1a079f063f464df737bcc82311d5ec1aa1a2e526
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-24 22:45:12 +0200

    code mistake corrected and change the correct name to the variable

--- src/api/ProductList.jsx
+++ src/api/ProductList.jsx
@@ -4,16 +4,16 @@ import Card from "../components/card/Card";
 import SearchBar from "../components/searchBar/SearchBar";
 const ProductList = () => {
   const { products } = useContext(ProductContext);
-  const { search, setSearch } = useState("");
-  const filterProducts = products.filter((product) => {
-    return product.title.trim().toLowerCase().includes(search.toLowerCase());
-  });
+  const [search, setSearch] = useState("");
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <>
       <SearchBar onSearch={setSearch} />
       <div className=" container max-w-sm p-10   md:max-w-xl lg:max-w-4xl  mx-auto">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {filterProducts.map((product) => {
+          {filteredProducts.map((product) => {
             return <Card key={product.id} product={product} />;
           })}
         </div>
