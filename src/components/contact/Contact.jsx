54c182c90c66d84d167d0a328aa9c91dfdc31d71
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-25 02:02:27 +0200

    form validation done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ValidForm from "../form/ValidForm";
-const map =
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2000.2682099722358!2d10.74988247714471!3d59.911095974901244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e8a1c253d39%3A0x6d69efbe96e850d2!2sOslo%20Sentralstasjon!5e0!3m2!1sno!2sno!4v1684880755876!5m2!1sno!2sno";
+
 const Contact = () => {
   return (
     <>
-      <iframe
-        title="map"
-        className="w-full h-60 max-w-xl mx-auto py-5"
-        src={map}
-      ></iframe>
-      <div className="container mx-auto max-w-lg">
-        <ValidForm />
-      </div>
+      <ValidForm />
     </>
   );
 };
--- src/components/form/ValidForm.jsx
+++ src/components/form/ValidForm.jsx
@@ -1,7 +1,93 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
+const schema = yup
+  .object({
+    firstName: yup
+      .string()
+      .min(3, "Your first name should be at least 3 characters.")
+      .required("Please enter your first name"),
+    subject: yup
+      .string()
+      .min(3, "Your subject should be at least 3 characters.")
+      .required("Please write your subject"),
+    email: yup
+      .string()
+      .email("email should be valid email.")
+      .required("Please write your valid email"),
+    message: yup
+      .string()
+      .min(3, "Your message should be at least 3 characters.")
+      .required("Please write your subject"),
+  })
+  .required();
 const ValidForm = () => {
-  return <div>ValidForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const submitForm = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className=" container max-w-lg mx-auto p-2">
+      <div>
+        <h1 className="text-2xl font-bold text-center py-3">Contact Us</h1>
+      </div>
+      <form
+        onSubmit={handleSubmit(submitForm)}
+        className="flex flex-col justify-center"
+      >
+        <input
+          className="border border-primary p-2 m-1 rounded-md "
+          type="text"
+          {...register("firstName")}
+          placeholder="First Name..."
+        />
+        <p className=" text-red-400 font-pins font-medium ">
+          {errors.firstName?.message}
+        </p>
+        <input
+          className="border border-primary p-2 m-1 rounded-md "
+          type="text"
+          {...register("subject")}
+          placeholder="Subject..."
+        />
+        <p className=" text-red-400 font-pins font-medium ">
+          {errors.subject?.message}
+        </p>
+        <input
+          className="border border-primary p-2 m-1 rounded-md "
+          type="text"
+          {...register("email")}
+          placeholder="Email..."
+        />
+        <p className=" text-red-400 font-pins font-medium ">
+          {errors.email?.message}
+        </p>
+        <textarea
+          className="border border-primary p-2 m-1 mb-4 rounded-md "
+          {...register("message")}
+          placeholder="Write your message..."
+          cols="30"
+          rows="5"
+        ></textarea>
+        <p className=" text-red-400 font-pins font-medium ">
+          {errors.message?.message}
+        </p>
+        <input
+          type="submit"
+          id="submit"
+          className=" lg:text-md md:text-md block  select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-2 border-primary my-3"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ValidForm;
