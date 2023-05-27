964a90970ef0bcf765b7b83c564b7ec8088e0243
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-28 01:39:12 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "420px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1192px",
+    },
     extend: {
       fontFamily: {
         pins: ["Poppins", "sans-serif"],
