af01339b60fc4c19c560d3a2bf6eef72c63d3b11
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-31 09:13:41 +0200

    update

--- src/components/detailCard/SingleItem.jsx
+++ src/components/detailCard/SingleItem.jsx
@@ -81,7 +81,7 @@ const SingleItem = ({ product }) => {
           {/* <Link to="/cart"> */}
           <button
             onClick={() =>
-              addToCart(product, id) && toast.success("Product added to cart")
+              addToCart(product, id) & toast.success("Product added to cart")
             }
             className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-2 border-primary "
           >
