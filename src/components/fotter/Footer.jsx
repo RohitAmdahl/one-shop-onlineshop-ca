c452e0f58eec87c6e9739296bddbf74de638b212
Rohit kumar <rohit_designer@outlook.com>
2023-06-05 20:17:39 +0200

    update footer

--- src/components/fotter/Footer.jsx
+++ src/components/fotter/Footer.jsx
@@ -4,12 +4,15 @@ const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <>
-      <footer className=" text-fontcolor font-pins ">
-        <div className=" container mx-auto max-w-md flex justify-center items-center ">
-          <img src={Logo} alt="logo" className=" p-5" />
+      <footer className=" text-fontcolor font-pins bg-yellow-100 max-w-2xl container mx-auto rounded-full shadow-md mb-6 my-6 ">
+        <div className=" container mx-auto max-w-md flex justify-center items-center   ">
+          <img src={Logo} alt="logo" className=" p-2" />
         </div>
         <div className="container mx-auto max-w-md flex justify-center items-center">
           <p> &copy; Rohit.K.A {year}</p>
+        </div>
+        <div className=" capitalize text-center py-2 ">
+           <p className=" text-amber-950 "> Build with create react App,tailwind CSS </p>
         </div>
       </footer>
     </>
