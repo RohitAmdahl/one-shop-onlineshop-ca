04cdd6a4d67a966488a5d169bf93821877851a3f
Rohit kumar <rohit_designer@outlook.com>
2023-06-06 13:29:07 +0200

    update

--- src/components/checkout/CheckOutCard.jsx
+++ src/components/checkout/CheckOutCard.jsx
@@ -1,31 +1,29 @@
-import React, { useContext } from "react";
+import React from "react";
 import FormatPrice from "../../helper/FormatPrice";
 
 const CheckOutCard = ({ CheckOutCardItem }) => {
   const { title, discountedPrice, imageUrl, amount } = CheckOutCardItem;
 
   return (
-    <>
-      <div className=" container mx-auto max-w-4xl my-6  border-b-2 pb-6">
-        <div className="flex flex-col gap-6  flex-wrap p-3 items-center  md:justify-around md:flex-row lg:justify-around lg:flex-row">
-          <img
-            src={imageUrl}
-            alt={title}
-            className=" max-w-[100px] rounded-lg "
-          />
-          <div className="flex flex-col ">
-            <h1 className="uppercase pb-4">{title}</h1>
-            <FormatPrice price={discountedPrice * amount} />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-center"> Quantity</p>
-            <div className="flex gap-5 pt-4 items-center  ">
-              <div className="font-bold text-2xl text-primary">{amount}</div>
-            </div>
+    <div className=" container mx-auto max-w-4xl my-6  border-b-2 pb-6">
+      <div className="flex flex-col gap-6  flex-wrap p-3 items-center  md:justify-around md:flex-row lg:justify-around lg:flex-row">
+        <img
+          src={imageUrl}
+          alt={title}
+          className=" max-w-[100px] rounded-lg "
+        />
+        <div className="flex flex-col ">
+          <h1 className="uppercase pb-4">{title}</h1>
+          <FormatPrice price={discountedPrice * amount} />
+        </div>
+        <div className="flex flex-col">
+          <p className="text-center"> Quantity</p>
+          <div className="flex gap-5 pt-4 items-center  ">
+            <div className="font-bold text-2xl text-primary">{amount}</div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,12 +5,14 @@ import CheckOutProduct from "../components/checkout/CheckOutProduct";
 
 const Checkout = () => {
   return (
-    <div className="lg:grid lg:grid-cols-2 lg:gap-14 max-w-4xl mx-auto container md:flex md:flex-wrap">
+    // <div className="lg:grid lg:grid-cols-2 lg:gap-14 max-w-4xl mx-auto container md:flex md:flex-wrap">
+    //   <div className="flex flex-col">
+    //   </div>
+    // </div>
+    <>
       <CheckOut />
-      <div className="flex flex-col">
-        <CheckOutProduct />
-      </div>
-    </div>
+      <CheckOutProduct />
+    </>
   );
 };
 
