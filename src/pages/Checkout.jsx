d9398df6631cc6123b1e6ecf6b2e182b2350a0e0
Rohit kumar <rohit_designer@outlook.com>
2023-06-06 13:33:43 +0200

    update checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import CheckOut from "../components/checkout/CheckOut";
-
 import CheckOutProduct from "../components/checkout/CheckOutProduct";
 
 const Checkout = () => {
   return (
-    // <div className="lg:grid lg:grid-cols-2 lg:gap-14 max-w-4xl mx-auto container md:flex md:flex-wrap">
-    //   <div className="flex flex-col">
-    //   </div>
-    // </div>
     <>
       <CheckOut />
       <CheckOutProduct />
